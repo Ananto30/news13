80f1d5f42b1d6617de1a737adb749ee5c4be7c15
Azizul Haque Ananto <azizulhaq.ananto@gmail.com>
2023-04-20 14:14:13 +0200

    Add header

--- web-app/src/App.svelte
+++ web-app/src/App.svelte
@@ -11,7 +11,7 @@
     fetch("https://news13.vercel.app/api/" + category + "?page=" + page)
       .then((data) => data.json())
       .then((data) => {
-        console.log(data)
+        console.log(data);
         newsList = [...newsList, ...data];
         fetching = false;
         page++;
@@ -29,24 +29,35 @@
 </script>
 
 <main class="container">
-  <h1>সর্বশেষ খবর​</h1>
-  <h6>
-    <!-- svelte-ignore a11y-missing-attribute -->
-    <a class="pointer" on:click={() => fetchCategory("all")}>সব</a>
-    |
-    <!-- svelte-ignore a11y-missing-attribute -->
-    <a class="pointer" on:click={() => fetchCategory("bangladesh")}>বাংলাদেশ</a>
-    (সূত্র: প্রথম আলো)
-  </h6>
-  <hr />
-  {#if newsList.length > 0}
-    <News bind:newsList />
-    <div class="center" use:inview={{}} on:change={fetchNews}>
-      <button on:click={fetchNews} disabled={fetching}>আরও খবর​</button>
-    </div>
-  {:else}
-    <p class="center">অপেক্ষা করুন​...</p>
-  {/if}
+  <div class="fixed">
+    <h6 class="inside">
+      <!-- svelte-ignore a11y-missing-attribute -->
+      <a class="pointer" on:click={() => fetchCategory("all")}>
+        <span class={category === "all" ? "underline" : ""}> সব </span>
+      </a>
+      |
+      <!-- svelte-ignore a11y-missing-attribute -->
+      <a class="pointer" on:click={() => fetchCategory("bangladesh")}>
+        <span class={category === "bangladesh" ? "underline" : ""}>
+          বাংলাদেশ
+        </span>
+      </a>
+      <span class="right font-light">(সূত্র: প্রথম আলো)</span>
+    </h6>
+  </div>
+
+  <div class="top-padding">
+    <h1>সর্বশেষ খবর​</h1>
+    <hr />
+    {#if newsList.length > 0}
+      <News bind:newsList />
+      <div class="center" use:inview={{}} on:change={fetchNews}>
+        <button on:click={fetchNews} disabled={fetching}>আরও খবর​</button>
+      </div>
+    {:else}
+      <p class="center">অপেক্ষা করুন​...</p>
+    {/if}
+  </div>
 </main>
 
 <style>
@@ -55,5 +66,37 @@
   }
   .pointer {
     cursor: pointer;
+  }
+
+  .fixed {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    background: #fff;
+    padding: 1rem;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  }
+
+  .inside {
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 0 4rem;
+  }
+
+  .top-padding {
+    padding-top: 3rem;
+  }
+
+  .underline {
+    text-decoration: underline;
+  }
+
+  .right {
+    float: right;
+  }
+
+  .font-light {
+    font-weight: 300;
   }
 </style>
